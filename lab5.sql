4015bccffe38fcae292f273e33ce528c4f0520e8
DariaF19 <80063167+DariaF19@users.noreply.github.com>
2022-04-22 14:53:01 +0300

    Add files via upload

---
+++ lab5.sql
@@ -0,0 +1,80 @@
+# 1 Добавление внешних ключей
+ALTER TABLE room_in_booking
+	ADD FOREIGN KEY(id_booking) REFERENCES booking(id_booking);
+ALTER TABLE booking
+	ADD FOREIGN KEY(id_client) REFERENCES client(id_client);
+ALTER TABLE room
+	ADD FOREIGN KEY(id_room_category) REFERENCES room_category(id_room_category);
+ALTER TABLE room
+	ADD FOREIGN KEY(id_hotel) REFERENCES hotel(id_hotel);
+ALTER TABLE room_in_booking
+	ADD FOREIGN KEY(id_room) REFERENCES room(id_room);
+
+# 2 Выдать информацию о клиентах гостиницы “Космос”, проживающих в номерах категории “Люкс” на 1 апреля 2019г
+SELECT client.name FROM client
+	LEFT JOIN booking ON booking.id_client = client.id_client
+	JOIN room_in_booking ON room_in_booking.id_booking = booking.id_booking
+	LEFT JOIN room ON room.id_room = room_in_booking.id_room
+	LEFT JOIN room_category ON room_category.id_room_category = room.id_room_category
+	LEFT JOIN hotel ON hotel.id_hotel = room.id_hotel
+	WHERE room_in_booking.checkin_date <= '2019-04-01' AND room_in_booking.checkout_date > '2019-04-01' AND room_category.name = 'Люкс' AND hotel.name = 'Космос';
+
+# 3 Дать список свободных номеров всех гостиниц на 22 апреля
+SELECT DISTINCT room.number, room.price, room_category.name, hotel.name FROM room 
+	LEFT JOIN room_category ON room_category.id_room_category = room.id_room_category
+	LEFT JOIN hotel ON hotel.id_hotel = room.id_hotel
+	LEFT JOIN room_in_booking ON room_in_booking.id_room = room.id_room
+	WHERE ((room_in_booking.checkin_date < '2019-04-22' AND room_in_booking.checkout_date <= '2019-04-22') OR
+		(room_in_booking.checkin_date > '2019-04-22'));
+
+# 4 Дать количество проживающих в гостинице “Космос” на 23 марта по каждой категории номеров
+SELECT room_category.name, COUNT(room_in_booking.id_room_in_booking) AS clients_count FROM room
+	LEFT JOIN room_category ON room_category.id_room_category = room.id_room_category
+	LEFT JOIN hotel ON hotel.id_hotel = room.id_hotel
+	LEFT JOIN room_in_booking ON room_in_booking.id_room = room.id_room
+	WHERE hotel.name = 'Космос' AND room_in_booking.checkin_date <= '2019-03-23' AND room_in_booking.checkout_date > '2019-03-23'
+	GROUP BY room_category.name;
+
+# 5 Дать список последних проживавших клиентов по всем комнатам гостиницы “Космос”, выехавшим в апреле с указанием даты выезда
+SELECT client.name, room.id_room, room_in_booking.checkout_date FROM room_in_booking
+	LEFT JOIN room ON room.id_room = room_in_booking.id_room
+	LEFT JOIN hotel ON hotel.id_hotel = room.id_hotel
+	LEFT JOIN room_category ON room_category.id_room_category = room.id_room_category
+	LEFT JOIN booking ON booking.id_booking = room_in_booking.id_booking
+	LEFT JOIN client ON client.id_client = booking.id_client
+	LEFT JOIN (SELECT room_in_booking.id_room, MAX(room_in_booking.checkout_date) AS last_date
+	FROM (SELECT * FROM room_in_booking WHERE checkout_date >='2019-04-01' AND checkout_date <= '2019-04-30') AS room_in_booking
+	GROUP BY room_in_booking.id_room) AS room_in_booking_2 ON room_in_booking_2.id_room = room_in_booking.id_room
+	WHERE hotel.name = 'Космос' AND room_in_booking_2.last_date = room_in_booking.checkout_date
+	GROUP BY room.id_room, client.name, room_in_booking.checkout_date;
+
+# 6 Продлить на 2 дня дату проживания в гостинице “Космос” всем клиентам комнат категории “Бизнес”, которые заселились 10 мая
+UPDATE room_in_booking
+	LEFT JOIN room ON room.id_room = room_in_booking.id_room
+	LEFT JOIN room_category ON room_category.id_room_category = room.id_room_category
+	LEFT JOIN hotel ON hotel.id_hotel = room.id_hotel
+	SET checkout_date = DATE_ADD(checkout_date, INTERVAL 2 DAY)
+	WHERE room_in_booking.checkin_date = '2019-05-10' AND room_category.name = 'Бизнес' AND hotel.name = 'Космос';
+
+#7 Найти все "пересекающиеся" варианты проживания
+SELECT * FROM room_in_booking rib1
+	INNER JOIN room_in_booking rib2 ON rib2.id_room_in_booking != rib1.id_room_in_booking AND
+	rib2.id_room = rib1.id_room
+	WHERE (rib1.checkin_date >= rib2.checkin_date AND rib1.checkin_date < rib2.checkout_date) OR
+	(rib2.checkin_date >= rib1.checkin_date AND rib2.checkin_date < rib1.checkout_date);
+
+# 8 Создать бронирование в транзакции
+START TRANSACTION;
+INSERT INTO client (name, phone) VALUES ('Иванов Степан Федорович', '+78536245123');
+INSERT INTO booking (id_client, booking_date)
+	(SELECT id_client, '2022-04-18' FROM client WHERE client.name = 'Семенова Анна Игоревна' AND client.phone = '+7902567585');
+INSERT INTO room_in_booking (id_booking, id_room, checkin_date, checkout_date)
+	(SELECT id_booking, 16, '2022-05-01', '2022-05-03' FROM booking WHERE booking.id_client =
+	(SELECT id_client FROM client WHERE client.name = 'Краснов Олег Егорович' AND client.phone = '+79987659231' LIMIT 1));
+COMMIT;
+
+# 9 Добавление необходимых индексов для всех таблиц
+CREATE INDEX index_room_in_booking_checkin_date ON room_in_booking(checkin_date);
+CREATE INDEX index_room_in_booking_checkout_date ON room_in_booking(checkout_date);
+CREATE INDEX index_room_category_name ON room_category(name);
+CREATE INDEX index_hotel_name ON hotel(name);
---
+++ lab6.sql
@@ -0,0 +1,60 @@
+# 1 Добавление внешних ключей
+ALTER TABLE dealer
+	ADD FOREIGN KEY(id_company) REFERENCES company(id_company);
+ALTER TABLE production
+	ADD FOREIGN KEY(id_company) REFERENCES company(id_company);
+ALTER TABLE `order`
+	ADD FOREIGN KEY(id_production) REFERENCES production(id_production);
+ALTER TABLE `order`
+	ADD FOREIGN KEY(id_pharmacy) REFERENCES pharmacy(id_pharmacy);
+ALTER TABLE production
+	ADD FOREIGN KEY(id_medicine) REFERENCES medicine(id_medicine);
+ALTER TABLE `order`
+	ADD FOREIGN KEY(id_dealer) REFERENCES dealer(id_dealer);
+
+# 2 Выдать информацию по всем заказам лекарства “Кордерон” компании “Аргус” с указанием названий аптек, дат, объема заказов
+SELECT pharmancy.name, `order`.date, `order`.quantity FROM `order`
+	LEFT JOIN pharmacy ON pharmancy.id_pharmacy = `order`.id_pharmacy
+	LEFT JOIN production ON production.id_production = `order`.id_production
+	LEFT JOIN company ON company.id_company = production.id_company
+	LEFT JOIN medicine ON medicine.id_medicine = production.id_medicine
+	WHERE medicine.name = 'Кордерон' AND company.name = 'Аргус';
+
+# 3 Дать список лекарств компании “Фарма”, на которые не были сделаны заказы до 25 января
+SELECT medicine.name FROM medicine WHERE medicine.id_medicine NOT IN (
+	SELECT medicine.id_medicine FROM medicine
+	LEFT JOIN production ON production.id_medicine = medicine.id_medicine
+	LEFT JOIN company ON company.id_company = production.id_company
+	INNER JOIN `order` ON `order`.id_production = production.id_production
+	WHERE company.name = 'Фарма'
+	GROUP BY medicine.id_medicine
+	HAVING (MIN(`order`.date) > '2019-02-25')
+	);
+
+# 4 Дать минимальный и максимальный баллы лекарств каждой фирмы, которая оформила не менее 120 заказов
+SELECT company.name, MIN(production.rating) AS min_rating, MAX(production.rating) AS max_rating FROM medicine
+	LEFT JOIN production ON production.id_medicine = medicine.id_medicine
+	LEFT JOIN company ON company.id_company = production.id_company
+	LEFT JOIN `order` ON `order`.id_production = production.id_production
+	GROUP BY company.name
+	HAVING COUNT(`order`.id_order) >= 120;
+
+# 5 Дать списки сделавших заказы аптек по всем дилерам компании “AstraZeneca”. Если у дилера нет заказов, в названии аптеки проставить NULL
+SELECT pharmacy.name, dealer.name FROM company
+	LEFT JOIN dealer ON dealer.id_company = company.id_company
+	LEFT JOIN `order` ON `order`.id_dealer = dealer.id_dealer
+	LEFT JOIN pharmacy ON pharmancy.id_pharmacy = `order`.id_pharmacy
+	WHERE company.name = 'AstraZeneca';
+
+# 6 Уменьшить на 20% стоимость всех лекарств, если она превышает 3000, а длительность лечения не более 7 дней
+UPDATE production
+	LEFT JOIN medicine ON medicine.id_medicine = production.id_medicine
+	SET price = (price * 0.8)
+	WHERE production.price > 3000 AND medicine.cure_duration <= 7;
+
+# 7 Добавление необходимых индексов
+CREATE INDEX index_production_price ON production(price);
+CREATE INDEX index_medicine_cure_duration ON medicine(cure_duration);
+CREATE INDEX index_company_name ON company(name);
+CREATE INDEX index_medicine_name ON medicine(name);
+CREATE INDEX index_order_date ON `order`(date);
